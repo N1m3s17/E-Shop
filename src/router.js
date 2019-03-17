79d02f5d883d758022b7a5ad68d3a585a80eb978
Semin Patel <seminspatel@gmail.com>
2019-03-17 16:49:55 -0400

    Added routes needed for Part one

--- src/router.js
+++ src/router.js
@@ -5,5 +5,33 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
+// Login Routes
+
+router.post('/login', (req, res) => {
+    res.send('Login');
+});
+
+router.post('/signup', (req, res) => {
+    res.send('Signup');
+});
+
+
+// Main Routes
+
+router.get('/', (req, res) => {
+    res.send('GET');
+});
+
+router.post('/item', (req, res) => {
+    res.send('POST');
+});
+
+router.put('/item/:id', (req, res) => {
+    res.send('PUT');
+});
+
+router.delete('/item/:id', (req, res) {
+    res.send('DESTROY');
+});
 
 module.exports = router;
