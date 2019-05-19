772248371f234fa739b2198a2e6729cb1fd06533
N1m3s17 <seminspatel@gmail.com>
2019-05-19 07:04:18 -0400

    Final push

--- server/db/index.js
+++ server/db/index.js
@@ -7,7 +7,7 @@ let writeFile = util.promisify(fs.writeFile);
 
 
 let itemsPath = path.resolve('server/db/items.json');
-let personal_infoPath = path.resolve('server/db/login_info.json');
+let usersPath = path.resolve('server/db/login_info.json');
 
 // READ AND WRITE TO ITEMS JSON
 
@@ -25,20 +25,44 @@ async function writeItems(items) {
     }
 
 async function createItem(item) {
-  let id = 0;
   return readItems()
     .then((items) => {
       // Ensures name is unique
       items.forEach((a) => {
-        if (a.name === item.name) {
-          // function won't excute including not executing countries.push(country);
+        if (a.name === item.name && a.id === item.id) {
+          // function won't excute including not executing items.push(item);
           throw new Error('Shoe already exists.');
-        }
-      id += 1;
-      
+        } 
       });
-      item.id = id;
       items.push(item);
+      return writeItems(items);
+    });
+}
+
+async function updateItemByName(id, updatedItem) {
+  return readItems()
+    .then((allItems) => {
+      return allItems.map((item) => {
+        if (item.id === id) {
+          return updatedItem;
+        } else {
+          return item;
+        }
+      });
+    })
+    .then((items) => {
+      return writeItems(items);
+    });
+}
+
+async function deleteItemById(id) {
+  return readItems()
+    .then((allItems) => {
+      return allItems.filter((item) => {
+        return item.id !== id;
+      });
+    })
+    .then((items) => {
       return writeItems(items);
     });
 }
@@ -46,22 +70,68 @@ async function createItem(item) {
   
  
     
-// READ AND WRITE TO PERSONAL INFO JSON
+// READ AND WRITE TO USERS JSON
 
-  async function readPersonal() {
-    return readFile(personal_infoPath)
+  async function readUsers() {
+    return readFile(usersPath)
       .then((contents) => {
         return JSON.parse(contents);
       });
   }
 
-  async function writePersonal(info) {
-    let json = JSON.stringify(info, null, 2);
-    return writeFile(itemsPath, json);
+  function writeUsers(users) {
+    return writeFile(usersPath, JSON.stringify(users, null, 2));
   }
   
-   
+  
+  
+  function usernameExists(username) {
+    return readUsers()
+      .then((users) => {
+        let exists = false;
+  
+        users.forEach((user) => {
+          if (user.username === username) {
+            exists = true;
+          }
+        });
+  
+        return exists;
+      });
+  }
+
+  function addUser(user) {
+    return readUsers()
+      .then((users) => {
+        return writeUsers(users.concat(user));
+      });
+  }  
+  
+  function getUserPasswordHash(username) {
+    return readUsers()
+      .then((users) => {
+        let match;
+  
+        users.forEach((user) => {
+          if (user.username === username) {
+            match = user;
+          }
+        });
+  
+        if (!match) {
+          throw new Error('User does not exist.');
+        }
+  
+        return match.password;
+      });
+  }
+  
   module.exports = {
     getAllItems: readItems,
     createItem: createItem,
+    updateItemByName: updateItemByName,
+    deleteItemById: deleteItemById,
+    addUser: addUser,
+    usernameExists: usernameExists,
+    getUserPasswordHash: getUserPasswordHash,
   };
--- server/router.js
+++ server/router.js
@@ -2,34 +2,140 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+let argon = require('argon2');
 const db = require('./db');
 
 const router = express.Router();
 
 // Login Routes
 
-router.post('/login', (req, res) => {
-    res.send('Login');
-    res.status(200);
+router.get('/login', (req, res) => {
+    res.render('login', {
+        pageId: 'login',
+        title: 'Login',
+        username: null,
+        formValues: { username: null, password: null },
+        formErrors: { username: null, password: null },
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    db.usernameExists(req.body.username)
+    .then((usernameExists) => {
+      // Login is not valid if username does not exist
+      if (!usernameExists) {
+        return false;
+      // If the username exists verify the password is correct
+      } else {
+        return db.getUserPasswordHash(req.body.username)
+          .then((dbHash) => {
+            return argon.verify(dbHash, req.body.password);
+          });
+      }
+    })
+    .then((isValid) => {
+      // If invalid respond with authentication failure
+      if (!isValid) {
+        res
+          .status(401)
+          .render('login', {
+            pageId: 'login',
+            title: 'Login',
+            username: req.body.username,
+            formError: 'Authentication failed.',
+            formValues: {
+              username: req.body.username || null,
+              password: req.body.password || null,
+            },
+            formErrors: { username: null, password: null },
+          });
+      // Else log the user in and redirect to home page
+      } else {
+        req.session.username = req.body.username
+        res.redirect('/');
+      }
+    })
+    .catch(next);
+});
+
+router.get('/logout', (req, res, next) => {
+    req.session.destroy((error) => {
+      if (error) {
+        next(error);
+       } else {
+        req.session.username = req.body.username
+        res.redirect('/');
+      }
+});
 });
 
 
-router.get('/signup', (req, res) => {
-    res.render('signup', {
-        pageId: 'signup',
-        title: 'Signup',
-        name: null,    
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        pageId: 'register',
+        title: 'Register',
+        username: null, 
+        formValues: { username: null, password: null },
+        formErrors: { username: null, password: null },
 });
 });
 
-router.post('/signup', (req, res) => {
-    res.send('Signup');
-    res.status(200);
-});
 
+router.post('/register', (req, res, next) => {
+    // First we check if the username provided already exists
+  db.usernameExists(req.body.username)
+  .then((usernameExists) => {
+    // Check if form values are valid
+    console.log(req.body, usernameExists);
+    let formErrors = {
+      username: (!usernameExists && req.body.username) ? null : 'Invalid username',
+      password: (req.body.password && req.body.password.length >= 6) ? null : 'Invalid password',
+    };
 
-// Main Routes
+    // If there are any errors do not register the user
+    if (formErrors.username || formErrors.password) {
+      res
+        .status(400)
+        .render('register', {
+          pageId: 'register',
+          title: 'Register',
+          username: null,
+          formErrors: formErrors,
+          formValues: {
+            username: req.body.username,
+            password: req.body.password,
+          },
+        });
+    // Else if the form values are valid
+    } else {
+      return argon.hash(req.body.password)
+        .then((dbHash) => {
+          let newUser = {
+            username: req.body.username,
+            password: dbHash,
+          };
 
+          return db.addUser(newUser);
+        })
+        .then(() => {
+          req.session.username = req.body.username
+          res.redirect('/success');
+        });
+    }
+  })
+  .catch(next);
+})
+
+router.get('/success', (req, res,) => {
+  res.render('success', {
+    pageId: 'success',
+    title: 'Home',
+    username: req.session.username,
+  })
+})
+
+// Main Routesnpm
 
 // This route is the homepage route which shows all shoes
 
@@ -40,6 +146,7 @@ router.get('/', (req, res, next) => {
             pageId: 'home',
             title: 'Home',
             shoes: shoes,
+            username: req.body.username,
         });   
     });
 });
@@ -53,8 +160,8 @@ router.get('/shoe/:id', (req, res) => {
         shoes.forEach((shoe) => {
             if(shoe.id === id){
                 res.render('shoebyid', {
-                    pageId: 'home',
-                    title: 'Home',
+                    pageId: 'shoebyid',
+                    title: 'Find Shoe',
                     shoe: shoe,
                 });
             };
@@ -69,14 +176,20 @@ router.get('/addshoe', (req, res) => {
     res.render('addshoe', {
         pageId: 'addshoe',
         title: 'Add a Shoe',
+        id: null,
         brand: null,
         name: null,
+        url: null,
+        description: null,
     });
 });
 
 router.post('/addshoe', (req, res, next) => {
+    let id = parseInt(req.body.id, 10);
     let brand = req.body.brand.trim();
     let name = req.body.name.trim();
+    let url = req.body.url.trim();
+    let description = req.body.description.trim();
 
     if (!name) {
       res
@@ -84,13 +197,19 @@ router.post('/addshoe', (req, res, next) => {
         .render('addshoe', {
           pageId: 'addshoe',
           title: 'Add a Shoe',
+          id: id,
           brand: brand,
           name: name,
+          url: url,
+          description: description,
         });
     } else {
       db.createItem({
+        id: id,
         brand: brand,
         name: name,
+        url: url,
+        description: description,
       })
         .then(() => {
           res.redirect(301, '/');
@@ -100,14 +219,57 @@ router.post('/addshoe', (req, res, next) => {
   });
 
   
-router.put('/shoe/:id', (req, res) => {
-    res.send('PUT');
-    res.status(200);
+  router.get('/shoe/:id/edit', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    db.getAllItems()
+    .then((shoes) => {
+        shoes.forEach((shoe) => {
+            if(shoe.id === id){
+                res.render('updateshoe', {
+                    pageId: 'updateshoe',
+                    title: 'Edit Shoe',
+                    shoe: shoe,
+                });
+            };
+        });
+    });
+
 });
 
-router.delete('/shoe/:id', (req, res) => {
-    res.send('DESTROY');
-    res.status(200);
+  router.post('/shoe/:id/edit', (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+    db.updateItemByName(id, req.body)
+      .then((shoes) => {
+        shoes.forEach((shoe) => {
+          if(shoe.id === id){
+              res.render('shoebyid', {
+                  pageId: 'shoebyid',
+                  title: 'Find Shoe',
+                  shoe: shoe,
+              });
+          };
+      });
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  });
+  
+  
+
+router.get('/removeshoe/:id', (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  db.deleteItemById(id)
+    .then((shoes) => {
+      res.render('home', {
+        pageId: 'home',
+        title: 'Home',
+        shoes: shoes,
+      });
+    })
+    .catch((error) => {
+      return next(error);
+    });
 });
 
 module.exports = router;
